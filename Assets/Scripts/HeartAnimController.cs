3b0ca413aa681d040fc1372b828335238805789b
b.mazuc <b.mazuc@student.isartdigital.com>
2020-02-01 18:46:15 +0100

    Add heart anim controller

---
+++ Assets/Scripts/HeartAnimController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class HeartAnimController : MonoBehaviour
+{
+    private Animator animator;
+    [SerializeField] private Gauge gauge;
+    [SerializeField] string parameter;
+    private int paramaterHashCode;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        gauge.OnValueChange += UpdateParameter;
+        paramaterHashCode = Animator.StringToHash(parameter);
+        UpdateParameter();
+    }
+
+    void UpdateParameter()
+    {
+        Debug.Log(gauge.Value);
+        animator.SetInteger(paramaterHashCode, gauge.Value);
+    }
+}
