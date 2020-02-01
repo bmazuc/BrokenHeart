6618f8e100264db0d4a722e50b3752f3235f6f55
b.mazuc <b.mazuc@student.isartdigital.com>
2020-02-01 19:16:45 +0100

    Add menu flow

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private Canvas menuInGame;
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void GoToMenuInGame()
+    {
+        menuInGame.gameObject.SetActive(true);
+    }
+
+    public void GoGame()
+    {
+        menuInGame.gameObject.SetActive(false);
+    }
+}
