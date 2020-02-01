c401bb81f05f0ff73b07ac2f6fefbca195525ad3
b.mazuc <b.mazuc@student.isartdigital.com>
2020-02-01 17:28:56 +0100

    Add base files and json

---
+++ Assets/Scripts/GaugeDisplayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GaugeDisplayer : MonoBehaviour
+{
+    [SerializeField] private Gauge gauge;
+    private Text text;
+    private string name;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+        name = text.text;
+        gauge.OnValueChange += DisplayValue;
+    }
+
+    void DisplayValue()
+    {
+        text.text = name + gauge.Value;
+    }
+}
---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Timeline place;
+    public Gauge selfEsteem;
+    public Gauge attention;
+
+    private void Start()
+    {
+        TextAsset json = Resources.Load<TextAsset>("timelines");
+        Debug.Log(json);
+        place = JsonUtility.FromJson<Timeline>(json.text);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            selfEsteem.Value += place.months[0].days[0].hour[0].ami.selfEsteem;
+            attention.Value += place.months[0].days[0].hour[0].ami.attention;
+        }
+    }
+}
